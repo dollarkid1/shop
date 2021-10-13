483719c4e2886feddcf6bffb70ce0e7c51b224ba
Humble Douglas <dollarkid17@gmail.com>
2021-10-13 10:36:15 +0100

    first commit

---
+++ src/test/java/com/ecommerce/shop/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.ecommerce.shop.data.repository;
+
+import com.ecommerce.shop.data.model.Currency;
+import com.ecommerce.shop.data.model.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Slf4j
+@Sql(scripts = {"/db/insert.sql"})
+class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepositoryImpl;
+
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    public void createProductTest(){
+        Product product = new Product();
+        product.setName("Luxury Sofa");
+        product.setPrice(400D);
+        product.setCurrency(Currency.NGN);
+        product.setDetails("the sofa in town newel");
+        assertThat(product).isNotNull();
+        assertThat(product.getId()).isNull();
+        log.info("Product before saving --> {}",product);
+        productRepositoryImpl.save(product);
+        assertThat(product.getId()).isNotNull();
+        log.info("Product after saving --> {}",product);
+    }
+    @Test
+    @DisplayName("when find all product is called then product list is returned")
+    @Transactional
+    public void productListIsReturned(){
+        List<Product>products = productRepositoryImpl.findAll();
+        assertThat(products).hasSize(4);
+        log.info("Products returned from database --> {}", products);
+    }
+}
