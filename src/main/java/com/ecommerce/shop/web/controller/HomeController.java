deb18ae2af453af09aef7f6bd9f7ceaf1900f8a5
Humble Douglas <dollarkid17@gmail.com>
2021-10-25 15:14:37 +0100

    get products

--- src/main/java/com/ecommerce/shop/web/controller/HomeController.java
+++ src/main/java/com/ecommerce/shop/web/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.ecommerce.shop.web.controller;
 
+import com.ecommerce.shop.data.model.Product;
+import com.ecommerce.shop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    ProductService productServiceImpl;
 
     @GetMapping("/index")
-    public String getIndex(){
+    public String getIndex(Model model){
+        List<Product> productList = productServiceImpl.findAll();
+        model.addAttribute("products", productList);
         return "index";
     }
 
